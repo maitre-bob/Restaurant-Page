d340e3cb76bf24d4e66f8ff4f3702bd024735892
maître-bob <saintfortbobbarthez@gmail.com>
2026-02-14 14:11:58 -0500

    Impotation du MENU dans index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import "./style.css";
+
 import loadHome from "./Home.js";
-
-console.log("Projet Restaurant démarré");
-
 loadHome();
+
+import loadMenu from "./menu.js";
+
+const btnMenu = document.getElementById("btn-menu");
+
+btnMenu.addEventListener("click", () => {
+  loadMenu();
+});
+
