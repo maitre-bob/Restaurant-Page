a487954efec133b811e22a88f8109e2fa9ceba7d
maître-bob <saintfortbobbarthez@gmail.com>
2026-02-14 12:45:53 -0500

    ajouter le minimum code html et css pour tester puis la création du fichier gitignore.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./style.css";
 
 console.log("Projet Restaurant démarré");
+
+// Exemple : générer un titre
+const h1 = document.createElement('h1');
+h1.textContent = "Bienvenue au Restaurant Odin!";
+document.body.appendChild(h1);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// webpack.config.js
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
@@ -9,6 +8,10 @@ export default {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/restaurant.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -21,12 +24,10 @@ export default {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-      
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
-
     ],
   },
 };
