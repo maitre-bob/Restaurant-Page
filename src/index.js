fcfec5c00fd6f659ed0ef210d7ebb1632922a721
maître-bob <saintfortbobbarthez@gmail.com>
2026-02-19 11:55:12 -0500

    les boutons on un effet de chocolat qui coule en activation

--- src/index.js
+++ src/index.js
@@ -4,23 +4,36 @@ import loadHome from "./Home.js";
 import loadMenu from "./menu.js";
 import loadContact from "./contacts.js";
 
-// Charger l'accueil par défaut au chargement
-loadHome();
 
 // Sélection des boutons
 const btnAccueil = document.getElementById("btn-accueil");
 const btnMenu = document.getElementById("btn-menu");
 const btnContacts = document.getElementById("btn-contacts");
 
+function setActive(activeBtn) {
+  const buttons = document.querySelectorAll(".nav-btn");
+
+  buttons.forEach(btn => btn.classList.remove("active"));
+
+  activeBtn.classList.add("active");
+}
+
+// Charger l'accueil par défaut au chargement
+loadHome();
+setActive(btnAccueil);
+
 // Ajouter les listeners
 btnAccueil.addEventListener("click", () => {
   loadHome();
+  setActive(btnAccueil);
 });
 
 btnMenu.addEventListener("click", () => {
   loadMenu();
+  setActive(btnMenu);
 });
 
 btnContacts.addEventListener("click", () => {
-    loadContact();
+    loadContact(btnContacts);
+    setActive(btnContacts);
 });
